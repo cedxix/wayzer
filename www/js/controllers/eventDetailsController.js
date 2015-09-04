601c6b1df1f497cd41d0ebee7a8760b8247e17dc
Cedric D. Anidie AST <canidie@astek.fr>
2015-09-04 13:51:26 +0200

    Modify view

--- www/js/controllers/eventDetailsController.js
+++ www/js/controllers/eventDetailsController.js
@@ -8,33 +8,27 @@ app.controller('eventDetailsController',
 
     $http.get('json/data.json').success(
       function (data) {
-        console.log(data);
         $scope.events = data.events;
         $scope.eventId = $state.params.eId;
-        console.log($state.params.eId);
 
         $scope.formtatDate = function (timestamp, arg) {
           console.log(timestamp);
           return 'full' === arg ? moment(timestamp).format('llll') : moment(timestamp).format('ll');
         };
 
-        var getPromotor = function (id) {
+        function getPromotor (id) {
 
           for (var i = 0; i < data.promotors.length; i++) {
-            console.log("promotor : " + data.promotors[i].name);
-            if (data.promotors[i] === id) {
-              console.log("promotor FIND : " + id);
+//            console.log("promotor : " + data.promotors[i].name);
+            if (data.promotors[i].id == id) {                            
               return data.promotors[i];
             }
           }
 
         };
 
-
         var id = $state.params.eId;
         $scope.promotor = getPromotor(id);
-
-
 
       });
 
